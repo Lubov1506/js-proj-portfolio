f1c6a11db691f4acc85617669ef7404b99da8ebc
Dimka863 <dlysachenko98@gmail.com>
2024-04-17 20:37:10 +0200

    First completed version with JS & modal

---
+++ src/js/index.js
@@ -0,0 +1,1 @@
+import './work-together/work-together';
---
+++ src/js/work-together/work-together.js
@@ -0,0 +1,60 @@
+import axios from 'axios';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+axios.defaults.baseURL = 'https://portfolio-js.b.goit.study/api-docs';
+const workTogetherForm = document.querySelector('.work-together-form');
+const emailInput = document.querySelector('#work-together-email');
+const commentInput = document.querySelector('#work-together-comment');
+const message = document.querySelector('.email-text');
+const backdrop = document.querySelector('.backdrop');
+const modalWindow = document.querySelector('.work-together-modal-window');
+const closeModalBtn = document.querySelector('.work-together-modal-btn');
+workTogetherForm.addEventListener('submit', submitHandler);
+console.log(modalWindow);
+const onCloseBtn = event => {
+  backdrop.classList.remove('is-open');
+  modalWindow.classList.remove('window-is-open');
+  message.classList.add('correct-text');
+  emailInput.classList.add('correct-input');
+  message.textContent = 'Succes!';
+  setTimeout(() => {
+    message.classList.remove('correct-text');
+    emailInput.classList.remove('correct-input');
+  }, 2000);
+  closeModalBtn.removeEventListener('click', onCloseBtn);
+};
+
+function submitHandler(event) {
+  event.preventDefault();
+  if (commentInput.value.trim() === '') {
+    iziToast.show({
+      title: 'Error',
+      message: 'Wrong value in comment.',
+      backgroundColor: 'tomato',
+      theme: 'dark',
+      color: 'red',
+      position: 'bottomRight',
+    });
+    return;
+  }
+  axios
+    .post('/requests', {
+      email: emailInput.value,
+      comment: commentInput.value,
+    })
+    .then(() => {
+      backdrop.classList.add('is-open');
+      modalWindow.classList.add('window-is-open');
+      closeModalBtn.addEventListener('click', onCloseBtn);
+    })
+    .catch(() => {
+      message.classList.add('incorrect-text');
+      message.textContent = 'Invalid email, try again';
+      emailInput.classList.add('incorrect-input');
+      setTimeout(() => {
+        message.classList.remove('incorrect-text');
+        emailInput.classList.remove('incorrect-input');
+      }, 2000);
+    });
+}
