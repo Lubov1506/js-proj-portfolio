5f6dabe846cc3d08187b9547040d8fc0ae9a88b3
Artem Parashchuk <a.p.paraschuk@gmail.com>
2024-04-18 09:49:10 +0300

    feature header
    added anchor to linck

--- src/js/header/header.js
+++ src/js/header/header.js
@@ -1,8 +1,9 @@
 const menuListElem = document.querySelector('.header-nav-list')
 const menuOpenBtnElem = document.querySelector('.header-open-menu-btn')
+const menuMobileOpenBtn = document.querySelector('.header-btn-mobile-menu-open')
 
 function toggleClass() {
   return  menuListElem.classList.toggle('is-hidden')
-    
 }
 menuOpenBtnElem.addEventListener('click',toggleClass)
+// menuMobileOpenBtn.addEventListener('.click',  )
