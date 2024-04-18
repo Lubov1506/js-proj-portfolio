1c5f5b43cfb850491bd394d8865a8d41e8af33b3
Artem Parashchuk <a.p.paraschuk@gmail.com>
2024-04-18 18:12:36 +0300

    fixBug/ header.js, mobMenu.js

--- src/js/mobile-menu/mobMenu.js
+++ src/js/mobile-menu/mobMenu.js
@@ -1,9 +1,7 @@
 const closeMobMenuBtn = document.querySelector('.btn-close-mob-menu')
 const mobMenuElem = document.querySelector('.mobile-menu-bg')
 const openMobMenuBtn = document.querySelector('.header-btn-mobile-menu-open')
-
 const mobMenuList = document.querySelector('.mobile-nav-list')
-
 const mobileLink = document.querySelector('.mobile-link')
 
 const showMobMenu = () => {
@@ -11,24 +9,24 @@ const showMobMenu = () => {
 }
 
 const closeMobMenu = () => {
-     mobMenuElem.classList.remove('is-open')
+    mobMenuElem.classList.remove('is-open')
 }
 
-// mobMenuList.addEventListener('click', closeMobMenu)
-
-closeMobMenuBtn.addEventListener('click', closeMobMenu)
-
-openMobMenuBtn.addEventListener('click', showMobMenu)
-
-mobileLink.addEventListener('click', (event) => {
+const moveToAnchor = (event) => {
     event.preventDefault();
+
     const targetId = event.target.getAttribute('href').substring(1);
-    console.log(event.target.getAttribute('href'))
     const targetElement = document.getElementById(targetId);
+
     if (targetElement) {
         targetElement.scrollIntoView({
             behavior: 'smooth'
         });
         mobMenuElem.classList.remove('is-open');
     }
-});
+}
+
+mobMenuList.addEventListener('click', moveToAnchor);
+mobileLink.addEventListener('click', moveToAnchor);
+closeMobMenuBtn.addEventListener('click', closeMobMenu)
+openMobMenuBtn.addEventListener('click', showMobMenu)
