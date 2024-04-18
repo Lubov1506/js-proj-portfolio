934983a413f0648afb6e455efe0a8dffd1632684
Sergii Minko <minkosergii@gmail.com>
2024-04-18 18:11:09 +0200

    edit index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
-
-import './header/header'
-import './mobile-menu/mobMenu'
-import './about/accordion'
-import './about/swipper'
+import './header/header';
+import './mobile-menu/mobMenu';
+import './about/accordion';
+import './about/swipper';
 import './work-together/work-together';
+import './reviews/reviews';
