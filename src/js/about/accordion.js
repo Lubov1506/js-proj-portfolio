fbaab17cd8ebb41889ed713b069380b61a57e914
Yelyzaveta31 <Didenkoelizabeth@gmail.com>
2024-04-18 18:01:47 +0300

    Update accordion.js

--- src/js/about/accordion.js
+++ src/js/about/accordion.js
@@ -3,15 +3,13 @@ import 'accordion-js/dist/accordion.min.css';
 
 const accordionItems = document.querySelectorAll('.my-information-item');
 document.addEventListener('DOMContentLoaded', () => {
-  // Закриває всі акордеони, окрім першого, який має клас 'active' за замовчуванням
   for (let i = 1; i < accordionItems.length; i++) {
-    accordionItems[i].querySelector('.my-information-content').style.height = '0';
+    accordionItems[i].querySelector('.my-information-content').style.height =
+      '0';
   }
 
-  // Додає обробник подій для кожного елемента акордеону
   accordionItems.forEach(item => {
     item.addEventListener('click', () => {
-      // Закриває всі акордеони, які не мають клас 'active'
       for (let i = 0; i < accordionItems.length; i++) {
         if (accordionItems[i] !== item) {
           accordionItems[i].classList.remove('active');
@@ -21,7 +19,6 @@ document.addEventListener('DOMContentLoaded', () => {
         }
       }
 
-      // Змінює стан активності поточного елемента і розкриває/закриває його контент
       item.classList.toggle('active');
       const content = item.querySelector('.my-information-content');
       content.style.height = item.classList.contains('active')
