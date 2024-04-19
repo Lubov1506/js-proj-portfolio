640c1e6570eb6acacd5c907c41042ef5a4ed63ed
Artem Parashchuk <a.p.paraschuk@gmail.com>
2024-04-19 17:00:50 +0300

    show header after scroll

--- src/js/header/header.js
+++ src/js/header/header.js
@@ -1,17 +1,36 @@
 const menuListElem = document.querySelector('.header-nav-list')
 const menuOpenBtnElem = document.querySelector('.header-open-menu-btn')
-const heightWindow = 1000
+const headMenuFixEl = document.querySelector('.head-menu-fixed')
+const headerElem = document.querySelector('.header')
+
+const heightWindow = 800
+let lastScroll = 0
 
 
-const listenWindowScroll = () => {
+const toggleClassHidden = () => {
+ menuListElem.classList.toggle('is-hidden')
+}
+
+const listenWindowScrollHeader = () => {
+  let currentScroll = window.scrollY 
+  
   if(window.scrollY >= heightWindow) {
     menuListElem.classList.add('is-hidden')
   }
+
+  if(currentScroll <= 5) {
+    return headerElem.classList.remove('head-menu-fixed') 
+  }
+
+  if(currentScroll < lastScroll) {
+    headerElem.classList.add('head-menu-fixed')
+  } else {
+    headerElem.classList.remove('head-menu-fixed')
+  }
+
+  lastScroll = currentScroll
 }
 
-window.addEventListener('scroll', listenWindowScroll)
 
-const toggleClass = () => {
-  return  menuListElem.classList.toggle('is-hidden')
-}
-menuOpenBtnElem.addEventListener('click',toggleClass)
+window.addEventListener('scroll', listenWindowScrollHeader)
+menuOpenBtnElem.addEventListener('click',toggleClassHidden)
