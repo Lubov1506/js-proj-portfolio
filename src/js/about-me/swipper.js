31d91dc7d1eae42eaa7254a566e1cb64dd1f44da
Lubov Parfonova <87125430+Lubov1506@users.noreply.github.com>
2024-04-19 16:04:37 +0300

    about

--- src/js/about-me/swipper.js
+++ src/js/about-me/swipper.js
@@ -1,45 +1,31 @@
-// import Swiper JS
-import Swiper from 'swiper';
-// import Swiper styles
+
+import Swiper from 'swiper/bundle';
 import { Navigation } from 'swiper/modules';
-// import Swiper and modules styles
-import 'swiper/css';
-import 'swiper/css/navigation';
 
 
-
-const swiper = new Swiper('.swiper', {
-    slidesPerView: 1,
-  direction: 'horizontal',
+new Swiper('.swiper-about', {
+  slidesPerView: 1,
+  // direction: 'horizontal',
   loop: true,
-  modules: [ Navigation ],
+  modules:[Navigation],
 
   breakpoints: {
-    375: {
+    320: {
       slidesPerView: 2,
-      width: 260
+
     },
     768: {
       slidesPerView: 3,
-      width: 600
+
     },
     1440: {
       slidesPerView: 6,
-      width: 1200
-    }
+  
+    },
   },
   navigation: {
-    nextEl: '.swiper-button-next custom',
+    nextEl: '.swiper-about-next',
   },
-//   on: {
-//     slideChange: function ()  {
-//       if (this.isEnd) {
-//         this.slideToLoop(1); // Перемотка на початок списку
-//       } else if (this.isBeginning) {
-//         this.slideToLoop(this.slides.length - 2); // Перемотка на кінець списку
-//       }
-//     },
-//   },
 });
 
 
