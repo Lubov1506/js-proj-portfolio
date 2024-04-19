31f3c3a4885a42b9046e4157af0a22175a5e777e
Yelyzaveta31 <Didenkoelizabeth@gmail.com>
2024-04-19 17:35:35 +0300

    add css

--- src/js/about-me/swipper.js
+++ src/js/about-me/swipper.js
@@ -1,6 +1,16 @@
+<<<<<<< Updated upstream
 
 import Swiper from 'swiper/bundle';
 import { Navigation } from 'swiper/modules';
+=======
+// import Swiper JS
+import Swiper from 'swiper/bundle';
+// import Swiper styles
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+>>>>>>> Stashed changes
 
 
 const swiper = new Swiper('.swiper.about', {
@@ -10,15 +20,21 @@ const swiper = new Swiper('.swiper.about', {
   modules:[Navigation],
 
   breakpoints: {
-    320: {
+    375: {
+      width: 260,
       slidesPerView: 2,
       spaceBetween: 0,
     },
     768: {
       slidesPerView: 3,
+<<<<<<< Updated upstream
       spaceBetween: 0,
+=======
+      width: 600,
+>>>>>>> Stashed changes
     },
     1440: {
+      width: 1200,
       slidesPerView: 6,
       spaceBetween: 0,
     },
