e9a554af2ff76e3c371b3f4abfff514826bc0bb8
Sergii Minko <minkosergii@gmail.com>
2024-04-19 21:44:27 +0200

    keyboard swiper

--- src/js/reviews/reviews.js
+++ src/js/reviews/reviews.js
@@ -3,8 +3,10 @@ import axios from 'axios';
 import { createReviewsList } from './render-functions';
 import 'swiper/css/bundle';
 const urlapi = 'https://portfolio-js.b.goit.study/api/reviews';
+let reviewsswiper;
 
 const reviewlist = document.querySelector('.reviews-list');
+const swiperContainer = document.querySelector('.swiper-reviews');
 const prevbtnEl = document.querySelector('.js-btn-prev');
 4;
 console.dir(prevbtnEl);
@@ -14,6 +16,7 @@ const prevsvgbtn = document.querySelectorAll('.js-icon-prev');
 console.dir(prevsvgbtn);
 const nextsvgbtn = document.querySelectorAll('.js-icon-next');
 console.dir(nextsvgbtn);
+let theEnd = false;
 
 const fetchReviews = async url => {
   try {
@@ -29,12 +32,22 @@ fetchReviews(urlapi).then(reviews => {
   console.log('Отримані відгуки:', reviews);
   reviewlist.innerHTML = createReviewsList(reviews);
 
-  const reviewsswiper = new Swiper('.swiper-reviews', {
+  reviewsswiper = new Swiper('.swiper-reviews', {
     modules: Navigation,
     direction: 'horizontal',
+    observer: true,
+    observeParents: true,
     slidesPerView: 1,
     slidesPerGroup: 1,
     spaceBetween: 18,
+    keyboard: {
+      enabled: true,
+      onlyInViewport: true,
+      pageUpDown: true,
+      tabKeys: true,
+    },
+
+    grabCursor: true,
 
     breakpoints: {
       768: {
@@ -54,3 +67,34 @@ fetchReviews(urlapi).then(reviews => {
     },
   });
 });
+
+swiperContainer.addEventListener('keydown', function (event) {
+  if (event.keyCode === 9) {
+    if (isInViewport(swiperContainer)) {
+      event.preventDefault();
+      if (!reviewsswiper.isEnd && !theEnd) {
+        reviewsswiper.slideNext();
+        if (reviewsswiper.isEnd) {
+          prevbtnEl.focus();
+          theEnd = true;
+        }
+      } else if (theEnd && !reviewsswiper.isBeginning) {
+        reviewsswiper.slidePrev();
+        if (reviewsswiper.isBeginning) {
+          nextbtnEl.focus();
+          theEnd = false;
+        }
+      }
+    }
+  }
+});
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
