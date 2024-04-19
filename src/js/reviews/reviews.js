168fae7d7468f7be6186bf03b43254155f1a0461
Sergii Minko <minkosergii@gmail.com>
2024-04-19 19:08:20 +0200

    swiper

--- src/js/reviews/reviews.js
+++ src/js/reviews/reviews.js
@@ -1,23 +1,19 @@
-import Swiper from 'swiper';
+import Swiper from 'swiper/bundle';
 import axios from 'axios';
-import {createReviewsList} from './render-functions';
+import { createReviewsList } from './render-functions';
 import 'swiper/css/bundle';
 const urlapi = 'https://portfolio-js.b.goit.study/api/reviews';
 
 const reviewlist = document.querySelector('.reviews-list');
-
-var mySwiper = new Swiper('.swiper-container', {
-  // Налаштування слайдера
-  direction: 'horizontal',
-  slidesPerView: 'auto',
-  spaceBetween: 16,
-  loop: false,
-  // Додайте навігаційні кнопки
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
+const prevbtnEl = document.querySelector('.js-btn-prev');
+4;
+console.dir(prevbtnEl);
+const nextbtnEl = document.querySelector('.js-btn-next');
+console.dir(nextbtnEl);
+const prevsvgbtn = document.querySelectorAll('.js-icon-prev');
+console.dir(prevsvgbtn);
+const nextsvgbtn = document.querySelectorAll('.js-icon-next');
+console.dir(nextsvgbtn);
 
 const fetchReviews = async url => {
   try {
@@ -32,4 +28,29 @@ const fetchReviews = async url => {
 fetchReviews(urlapi).then(reviews => {
   console.log('Отримані відгуки:', reviews);
   reviewlist.innerHTML = createReviewsList(reviews);
+
+  const reviewsswiper = new Swiper('.swiper-reviews', {
+    modules: Navigation,
+    direction: 'horizontal',
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    spaceBetween: 18,
+
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 16,
+        slidesPerGroup: 1,
+      },
+      1440: {
+        slidesPerView: 4,
+        spaceBetween: 16,
+        slidesPerGroup: 1,
+      },
+    },
+    navigation: {
+      nextEl: '.js-btn-next',
+      prevEl: '.js-btn-prev',
+    },
+  });
 });
