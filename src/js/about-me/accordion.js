ffdf65b392e153533af5cfb03eb4022d5f680df3
Yelyzaveta31 <Didenkoelizabeth@gmail.com>
2024-04-19 12:22:35 +0300

    update css + accordion

--- src/js/about-me/accordion.js
+++ src/js/about-me/accordion.js
@@ -1,5 +1,7 @@
 import Accordion from 'accordion-js';
 
 const acContainer = document.querySelector('.accordion-container');
+acContainer.children[0].classList.add('is-active');
 console.log(acContainer);
-const acc = new Accordion(acContainer);
+new Accordion(acContainer, {
+openOnInit:[0]});
