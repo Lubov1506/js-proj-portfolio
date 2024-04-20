104bc2aa62208633acfdd0447295692ce77839fb
Yelyzaveta31 <Didenkoelizabeth@gmail.com>
2024-04-20 08:51:32 +0300

    update swipper

--- src/js/about-me/swipper.js
+++ src/js/about-me/swipper.js
@@ -5,26 +5,39 @@ import { Navigation } from 'swiper/modules';
 
 const swiper = new Swiper('.swiper.about', {
   direction: 'horizontal',
-  slidesPerView: 2,
+  spaceBetween: 0,
   loop: true,
   modules:[Navigation],
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+  mousewheel: {
+    invert: true,
+  },
+
+  navigation: {
+    nextEl: '.swiper-button-next-about',
+  },
   breakpoints: {
     320: {
       width: 260,
+      slidesPerView: 2,
+      slidesOffsetAfter: 130,
     },
     768: {
       slidesPerView: 3,
       width: 600,
+      slidesOffsetAfter: 200,
     },
     1440: {
       width: 1200,
       slidesPerView: 6,
+      slidesOffsetAfter: 200,
   
     },
   },
-  navigation: {
-    nextEl: '.swiper-button-next-about',
-  },
+  
 });
 
 
