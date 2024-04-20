1b69d75b64f95877a18a624125f4ad80feb4a810
Dragon2000v <shmatko064@gmail.com>
2024-04-20 12:08:50 +0300

    fix style projects . fix svg .add key and wheel  event

--- src/js/projects/projects.js
+++ src/js/projects/projects.js
@@ -3,6 +3,18 @@ import 'swiper/css/bundle';
 
 new Swiper('.projectSwiper', {
   spaceBetween: 30,
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+    tabKeys: true,
+  },
+  
+  mousewheel: {
+    invert: true,
+  },
+
+  grabCursor: true,
 
   navigation: {
     nextEl: '.js-btn_arrow-right',
