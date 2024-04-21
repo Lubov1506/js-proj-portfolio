1a010d53d604c1cd72cef16fe4caa74a9f729271
Lubov Parfonova <87125430+Lubov1506@users.noreply.github.com>
2024-04-21 21:41:37 +0300

    format

--- src/js/about-me/accordion.js
+++ src/js/about-me/accordion.js
@@ -5,5 +5,5 @@ const acContainer = document.querySelector('.accordion-container');
 acContainer.children[0].classList.add('is-active');
 
 new Accordion(acContainer, {
-openOnInit:[0]});
-
+  openOnInit: [0],
+});
